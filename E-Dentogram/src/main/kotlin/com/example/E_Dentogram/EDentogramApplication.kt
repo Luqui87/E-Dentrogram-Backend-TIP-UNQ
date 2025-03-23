7308ec59fb1d4b284d84890158002e007f4385ae
JuanIgnGarcia <juan.ignacio.garcia.02@gmail.com>
2025-03-23 13:17:41 -0300

    set up inicial

---
+++ E-Dentogram/src/main/kotlin/com/example/E_Dentogram/EDentogramApplication.kt
@@ -0,0 +1,11 @@
+package com.example.E_Dentogram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EDentogramApplication
+
+fun main(args: Array<String>) {
+	runApplication<EDentogramApplication>(*args)
+}
---
+++ E-Dentogram/src/main/kotlin/com/example/E_Dentogram/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.example.E_Dentogram
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(EDentogramApplication::class.java)
+	}
+
+}
