535020c34eacc3a31b732c8e0a3659d4c4cd3197
Lucas Alvarez <alvarezlucas2787@gmail.com>
2025-03-30 12:09:50 -0300

    Testing all endpoints Patient Controller

--- src/test/kotlin/com/example/E_Dentogram/controller/PatientControllerTest.kt
+++ src/test/kotlin/com/example/E_Dentogram/controller/PatientControllerTest.kt
@@ -2,6 +2,7 @@ package com.example.E_Dentogram.controller
 
 import com.example.E_Dentogram.model.Patient
 import com.example.E_Dentogram.repository.PatientRepository
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.hamcrest.collection.IsCollectionWithSize
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
@@ -85,9 +87,67 @@ class PatientControllerTest {
     }
 
     @Test
-    fun `should get not get specific patient`(){
+    fun `should not get specific patient`(){
+
+        mockMVC.perform(get("/patient/123"))
+            .andDo(print())
+            .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun `should get the specific patient`(){
+
+        val patient = Patient.PatientBuilder()
+            .medicalRecord(123)
+            .dni(42421645)
+            .name("Lucas Alvarez")
+            .address("Bragado 1413")
+            .birthdate(LocalDate.of(2000, 10, 12))
+            .telephone(1153276406)
+            .email("lucas@mail.com")
+            .build()
+
+        patientRepository.save(patient)
+
+        mockMVC.perform(get("/patient/123"))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.medicalRecord").value(123))
+            .andExpect(jsonPath("$.dni").value(42421645))
+            .andExpect(jsonPath("$.name").value("Lucas Alvarez"))
+            .andExpect(jsonPath("$.address").value("Bragado 1413"))
+            .andExpect(jsonPath("$.birthdate").value("2000-10-12"))
+            .andExpect(jsonPath("$.telephone").value(1153276406))
+            .andExpect(jsonPath("$.email").value("lucas@mail.com"))
+            .andExpect(jsonPath("$.teeth", IsCollectionWithSize.hasSize<Array<Any>>(0)))
 
     }
 
+    @Test
+    fun `should create a patient`(){
+        var body = mapOf(
+            "medicalRecord" to "123",
+            "dni" to "42421645",
+            "name" to "Lucas Alvarez",
+            "address" to "Bragado 1413",
+            "birthdate" to "2000-10-12",
+            "telephone" to "1153276406",
+            "email" to "lucas@mail.com"
+        )
+
+        mockMVC.perform(post("/patient/123)")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jacksonObjectMapper().writeValueAsString(body)))
+
+            .andDo(print())
+            .andExpect(status().isCreated)
+            .andExpect(jsonPath("$.medicalRecord").value(123))
+            .andExpect(jsonPath("$.dni").value(42421645))
+            .andExpect(jsonPath("$.name").value("Lucas Alvarez"))
+            .andExpect(jsonPath("$.address").value("Bragado 1413"))
+            .andExpect(jsonPath("$.birthdate").value("2000-10-12"))
+            .andExpect(jsonPath("$.telephone").value("1153276406"))
+            .andExpect(jsonPath("$.email").value("lucas@mail.com"))
+    }
 
 }
