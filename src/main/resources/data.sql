beaf466ad028eb8b0953061b9e9526b872e2d440
Lucas Alvarez <alvarezlucas2787@gmail.com>
2025-03-30 11:17:42 -0300

    Testing GET allPatients and populate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO patient_table (medical_record, dni, name, address, birthdate, telephone, email) VALUES
+                          (312, 425940982,'Lucas Alvarez', 'Bragado 1947', '2000-10-12', 1153276406,'alvarezlucas@gmail.com'),
+                         (14, 42133192,'Juan Garcia', 'Chascomus 1312', '2000-01-10', 1151471402,'garciajuan@gmail.com');
---
+++ src/test/kotlin/com/example/E_Dentogram/controller/PatientControllerTest.kt
@@ -0,0 +1,93 @@
+package com.example.E_Dentogram.controller
+
+import com.example.E_Dentogram.model.Patient
+import com.example.E_Dentogram.repository.PatientRepository
+import org.hamcrest.collection.IsCollectionWithSize
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.time.LocalDate
+
+@Testcontainers
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PatientControllerTest {
+
+
+    @Autowired
+    private lateinit var mockMVC : MockMvc
+
+    @Autowired
+    private lateinit var patientRepository: PatientRepository
+
+    companion object{
+        @Container
+        private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:latest")
+
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgreSQLContainer::getUsername)
+            registry.add("spring.datasource.password", postgreSQLContainer::getPassword)
+        }
+
+    }
+
+    @BeforeEach
+    fun setup() {
+        patientRepository.deleteAll()
+    }
+
+    @Test
+    fun `should get no patients `(){
+        mockMVC.perform(get("/allPatients"))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$", IsCollectionWithSize.hasSize<Array<Any>>(0)))
+    }
+
+    @Test
+    fun `should get one patient`(){
+        val patient = Patient.PatientBuilder()
+            .medicalRecord(123)
+            .dni(42421645)
+            .name("Lucas Alvarez")
+            .address("Bragado 1413")
+            .birthdate(LocalDate.of(2000, 10, 12))
+            .telephone(1153276406)
+            .email("lucas@mail.com")
+            .build()
+
+        patientRepository.save(patient)
+
+        mockMVC.perform(get("/allPatients"))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$", IsCollectionWithSize.hasSize<Array<Any>>(1)))
+            .andExpect(jsonPath("$[0].medicalRecord").value(123))
+            .andExpect(jsonPath("$[0].name").value("Lucas Alvarez"))
+            .andExpect(jsonPath("$[0].dni").value(42421645))
+    }
+
+    @Test
+    fun `should get not get specific patient`(){
+
+    }
+
+
+}
