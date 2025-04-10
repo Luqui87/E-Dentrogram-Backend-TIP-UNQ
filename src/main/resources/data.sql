9c5e51910f3c13debf620f25f5f445e4a7dfb008
Lucas Alvarez <alvarezlucas2787@gmail.com>
2025-04-10 19:32:12 -0300

    Added ddl-auto to update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,11 @@ INSERT INTO patient_table (medical_record, dni, name, address, birthdate, teleph
                         (318, 412345678, 'Carlos Fernández', 'Mitre 789', '1988-09-14', 1167890123, 'carlos.fernandez@gmail.com'),
                         (507, 437654321, 'Ana Martínez', 'San Martín 456', '1992-12-30', 1156789012, 'ana.martinez@gmail.com'),
                         (623, 405678912, 'Sofía Ramírez', 'Corrientes 333', '1999-07-18', 1176543289, 'sofia.ramirez@gmail.com'),
-                        (731, 428901234, 'Diego Torres', 'Belgrano 220', '1985-03-05', 1134567890, 'diego.torres@gmail.com');
+                        (731, 428901234, 'Diego Torres', 'Belgrano 220', '1985-03-05', 1134567890, 'diego.torres@gmail.com')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO tooth_table (patient_id, "number", "center", "down", "left", "right", "up")
                 VALUES (215, 1, 0, 0, 2, 0, 0),
                          (215,2,2,0,2, 1, 0),
-                         (215,23,1,0,2, 1, 0);
+                         (215,23,1,0,2, 1, 0)
+ON CONFLICT DO NOTHING;
--- src/test/kotlin/com/example/E_Dentogram/controller/PatientControllerTest.kt
+++ src/test/kotlin/com/example/E_Dentogram/controller/PatientControllerTest.kt
@@ -12,7 +12,6 @@ import org.mockito.kotlin.any
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.DynamicPropertyRegistry
